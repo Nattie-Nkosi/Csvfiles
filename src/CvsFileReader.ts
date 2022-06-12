8ef7720add44d7ecd4533cd7eea9c7f32393fda7
Nutty4616 <nkosin361@gmail.com>
2022-06-12 22:09:51 +0200

    Introducing Generics, defining reusable clasess

--- src/CvsFileReader.ts
+++ src/CvsFileReader.ts
@@ -1,11 +1,11 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils';
-import { MatchResult } from './MatchResult';
 
-class CsvFileReader {
-  data: string[][] = [];
+abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) { }
+
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs.readFileSync(this.filename, {
@@ -14,18 +14,10 @@ class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(',')
       })
-      .map((row: string[]): any => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult, // Type assertion
-          row[6]
-        ];
-      })
+      .map(this.mapRow)
   }
+
+
 }
 
 export { CsvFileReader }
